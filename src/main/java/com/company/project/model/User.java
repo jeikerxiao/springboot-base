2c35af3d0c953e5daa79c7e31aab265e39b80e15
jeikerxiao <jeiker@126.com>
2018-05-16 10:17:50 +0800

    add swagger

--- src/main/java/com/company/project/model/User.java
+++ src/main/java/com/company/project/model/User.java
@@ -1,112 +1,38 @@
 package com.company.project.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import java.util.Date;
 import javax.persistence.*;
 
+@Data
+@ApiModel("用户基本信息")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @ApiModelProperty("用户名")
     @NotBlank(message = "用户名不能为空")
     private String username;
 
+    @ApiModelProperty("密码")
     @NotBlank(message = "密码不能为空")
     private String password;
 
+    @ApiModelProperty("用户昵称")
     @NotBlank(message = "用户昵称不能为空")
     @Column(name = "nick_name")
     private String nickName;
 
+    @ApiModelProperty("性别")
     private Integer sex;
 
+    @ApiModelProperty("注册日期")
     @Column(name = "register_date")
     private Date registerDate;
-
-    /**
-     * @return id
-     */
-    public Integer getId() {
-        return id;
-    }
-
-    /**
-     * @param id
-     */
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    /**
-     * @return username
-     */
-    public String getUsername() {
-        return username;
-    }
-
-    /**
-     * @param username
-     */
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    /**
-     * @return password
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * @param password
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    /**
-     * @return nick_name
-     */
-    public String getNickName() {
-        return nickName;
-    }
-
-    /**
-     * @param nickName
-     */
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    /**
-     * @return sex
-     */
-    public Integer getSex() {
-        return sex;
-    }
-
-    /**
-     * @param sex
-     */
-    public void setSex(Integer sex) {
-        this.sex = sex;
-    }
-
-    /**
-     * @return register_date
-     */
-    public Date getRegisterDate() {
-        return registerDate;
-    }
-
-    /**
-     * @param registerDate
-     */
-    public void setRegisterDate(Date registerDate) {
-        this.registerDate = registerDate;
-    }
 }
